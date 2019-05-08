200b0439c42f79ac4846d9ee5937373ad19a3c5a
hyelim3316@gmail.com <hyelim3316@gmail.com>
2019-05-07 20:15:08 -0400

    first

---
+++ app/data/friends.js
@@ -0,0 +1,87 @@
+var friendslist =[
+
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"Andy",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"Heylim",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"Paul",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        4,
+        4,
+        4,
+        5,
+        2,
+        4,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"Ed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        2,
+        3,
+        4,
+        5,
+        3,
+        2,
+        2,
+        5,
+        1,
+        1
+      ]
+  }]
+  module.exports = friendslist;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var friendsList = require("../data/friends");
+
+module.exports = function (app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsList);
+    });
+    app.post("/api/friends", function(req, res) {
+        console.log(req.body)
+        req.body.score
+        
+
+        // friendData.push(req.body);
+        res.json(friendsList);
+    });
+};
