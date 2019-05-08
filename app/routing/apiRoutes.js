c6460e91244cdc823bcd02c836ce4c64b52329c4
hyelim3316@gmail.com <hyelim3316@gmail.com>
2019-05-07 23:07:20 -0400

    upadated modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,10 +6,18 @@ module.exports = function (app) {
     });
     app.post("/api/friends", function(req, res) {
         console.log(req.body)
-        req.body.score
+        for (var i = 0; i < friendsList.length ; i++){
+            friendsList[i].scores
+            for (var j = 0; j < 10; j++){
+                var difference = friendsList[i].scores[j]-req.body.score[j]
+            }
+        }
         
-
+        
+        
         // friendData.push(req.body);
-        res.json(friendsList);
+        res.json({
+            name : 'Ant'
+        });
     });
 };
